47d355e455b5e05d5ec93277367a64797767d835
devzota <111525617+devzota@users.noreply.github.com>
2023-05-11 10:17:06 +0700

    hào 301
    301

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -11,8 +11,13 @@
  */
 export default defineEventHandler((event) => {
     const headers = event.req.headers;
-    
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://atoz24h.info"+event.req.url);
+        if(event.req.url.includes('d%d0%b5mi-rose-bar%d0%b5s-her-b%e1%b4%9cm-in-s%d0%b5%d0%b5-thro%e1%b4%9cgh-dr%d0%b5ss-as-she-mod%d0%b5ls-her-own-bi%d0%baini-rang%d0%b5')){
+            sendRedirect(event,"https://newslife247.site/d%d0%b5mi-rose-bar%d0%b5s-her-b%e1%b4%9cm-i%d0%bf-s%d0%b5%d0%b5-thro%e1%b4%9cgh-dr%d0%b5ss-as-she-mod%d0%b5ls-her-ow%d0%bf-bi%d0%bai%d0%bfi-ra%d0%bfg%d0%b5?utm_medium=Kate_Hudson_Lover&utm_source=Hao_10/05");
+        }
+        else{
+            sendRedirect(event,"https://atoz24h.info"+event.req.url);
+        }
+        
     }
 })
